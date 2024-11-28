c37456cabb5a5dacea67501bb4ef2d238f721ad8
robrtoRR <81438850+robrtoRR@users.noreply.github.com>
2024-11-27 19:57:15 -0600

    amplifyconfiguration

---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,33 @@
+const amplifyconfig = ''' {
+  "UserAgent": "aws-amplify-cli/2.0",
+  "Version": "1.0",
+  "auth": {
+    "plugins": {
+      "awsCognitoAuthPlugin": {
+        "IdentityManager": {
+          "Default": {}
+        },
+        "CognitoUserPool": {
+          "Default": {
+            "PoolId": "us-east-1_FFvAfQ4Z0",
+            "AppClientId": "148fj2lh7o0s20fcp3mp6c0jrh",
+            "Region": "us-east-1"
+          }
+        },
+         "Auth": {
+          "Default": {
+            "authenticationFlowType": "USER_SRP_AUTH",
+            "usernameAttributes": ["email"],
+            "signupAttributes": [
+              "email", "name"
+             ],
+            "passwordProtectionSettings": {
+                "passwordPolicyMinLength": 8,
+                "passwordPolicyCharacters": []
+            }
+          }
+        }
+      }
+    }
+  }
+}''';
